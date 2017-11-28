0d54d45c87e69a2f98095cbe8d0f8abf9d61c775
Chris Ryan <chris.ryan@ctc.usyd.edu.au>
2017-11-29 10:28:18 +1100

    file cleanup

---
+++ README.md
@@ -0,0 +1,25 @@
+# docdeck
+
+A command line app for generating an HTML wiki from markdown files
+
+## Installation
+
+1. Grab the code from the github repo
+
+        npm install
+
+2. cd into the project directory
+
+        cd dockdeck
+
+3. Install dependencies
+
+        npm install
+
+4. link to be able to run globally
+
+        npm link
+
+## Usage
+
+docdeck build <location of folder containing markdown files>
---
+++ docdeck.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+
+// commnander - command line options and arguments
+program.version('0.1.0')
+    .command('build <src>', 'build HTML from supplied source directory')
+    //.option('-b, --build', 'build html')
+    //.arguments('<cmd> [src] [dest]', 'build HTML from supplied source folder')
+    //.action(function(srcFolder, command){
+        //src = srcFolder;
+      //  console.log(command.args[0]);
+        //destFolder = dest;
+    //})
+    .parse(process.argv);
